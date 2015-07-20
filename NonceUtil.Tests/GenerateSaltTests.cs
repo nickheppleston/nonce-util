3eba18b3a69224735650b3ca6179a74170564018
Nick Heppleston <nick@modhul.com>
2015-07-20 13:32:42 +0100

    Implementation of the internal GenerateSalt method, plus supporting unit-tests.

---
+++ NonceUtil.Tests/GenerateSaltTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace NonceUtil.Tests
+{
+    [TestClass]
+    public class GenerateSaltTests
+    {
+        [TestMethod]
+        public void GenerateSalt_Single()
+        {
+            // Setup
+            var saltLength = 15;
+
+            // Execution
+            var salt = NonceUtil.GenerateSalt(saltLength);
+
+            // Assertion
+            Assert.IsFalse(String.IsNullOrEmpty(salt));
+            Assert.AreEqual(saltLength, salt.Length);
+        }
+
+        [TestMethod]
+        public void GenerateSalt_Multiple()
+        {
+            // Setup
+            var saltLength = 15;
+            var saltCount = 100;
+            var saltList = new List<string>();
+
+            // Execution
+            for (int i = 0; i < saltCount; i++)
+            {
+                var salt = NonceUtil.GenerateSalt(saltLength);
+
+                if (!saltList.Contains(salt))
+                    saltList.Add(salt);
+            }
+
+            // Assertion
+            Assert.AreEqual(saltCount, saltList.Count);
+        }
+    }
+}
---
+++ NonceUtil/NonceUtil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonceUtil
+{
+    public static class NonceUtil
+    {
+        private const string SALT_CHARS = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+
+        private static Random rand = new Random();
+
+        public static string GenerateNonce()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool CheckNonce()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static string GenerateSalt(int saltLength)
+        {
+            var salt = new StringBuilder();
+            
+            var chars = SALT_CHARS.ToCharArray();
+		    var ll = chars.Length - 1;
+
+            while (salt.Length < saltLength)
+                salt.Append(chars[rand.Next(0, ll)]);
+
+            return (salt.ToString());
+        }
+    }
+}
